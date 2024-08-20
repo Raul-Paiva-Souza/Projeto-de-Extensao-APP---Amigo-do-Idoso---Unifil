2f5d15b5775c6e238afeba44676bb9e1a81d3d0e
Raul <umtalderaul@gmail.com>
2024-08-19 21:50:36 -0300

    Adiciona novo arquivo

---
+++ app/src/main/java/com/example/amigo_do_idoso_v31/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.amigo_do_idoso_v31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Amigo_do_Idoso_v3.1"
+include(":app")
